3c9f76feb76da3b1166c4c4c548f6e3db7d4f385
pamir <pamirghimire@gmail.com>
2019-12-11 22:06:49 +0100

    Adds estimation of epipolar geometry, app for fund. mat estimation on stream

--- apps/monoStreamFundamentalMatEstimation_multiThreaded.cpp
+++ apps/monoStreamFundamentalMatEstimation_multiThreaded.cpp
@@ -1,6 +1,7 @@
 #include "FeatureMatcher.h"
+#include "StereoGeometer.h"
 #include "TimelapseCamera.h"
-#include <chrono>
+
 #include <thread>
 
 struct AppData {
@@ -11,8 +12,6 @@ static AppData app_data;
 
 void UpdateDataFromTimelapseCamera() {
   using namespace cvp::vision;
-  using namespace std::chrono_literals;
-
   const auto number_of_frames_to_skip_between_matched_frames = 5;
   TimelapseCamera timelapse_camera(
       number_of_frames_to_skip_between_matched_frames);
@@ -20,7 +19,6 @@ void UpdateDataFromTimelapseCamera() {
   while (app_data.app_is_alive_) {
     timelapse_camera.Update();
     app_data.time_separated_frames_ = timelapse_camera.GetTimeSeparatedFrames();
-    // std::this_thread::sleep_for(10ms);
   }
 }
 
@@ -73,7 +71,15 @@ bool UserPressedEscapeKey() {
 }
 
 void DrawFeatureMatchesOnMonocularStream() {
+  using namespace cvp::vision;
+  const auto number_of_frames_to_skip_between_matched_frames = 5;
+  TimelapseCamera timelapse_camera(
+      number_of_frames_to_skip_between_matched_frames);
+
   while (true) {
+    timelapse_camera.Update();
+    app_data.time_separated_frames_ = timelapse_camera.GetTimeSeparatedFrames();
+
     if (app_data.time_separated_frames_.IsValid())
       ShowMatchesBetweenTimeSeparatedFrames();
 
@@ -84,9 +90,49 @@ void DrawFeatureMatchesOnMonocularStream() {
   }
 }
 
+cv::Mat EstimateFundamentalMatrixFromMatches(
+    const cvp::vision::visual_features::CorrespondingKeypointsInImagePair &matches_between_images) {
+  using namespace cvp::vision;
+  geometry::StereoGeometer stereo_geometer;
+  const auto epipolar_geometry_from_matches =
+      stereo_geometer.EstimateFundamentalMatrixFromStereoMatches(
+        matches_between_images.keypoints_left_image_,
+        matches_between_images.keypoints_right_image_
+      );
+
+  return epipolar_geometry_from_matches;
+}
+
+cvp::vision::visual_features::CorrespondingKeypointsInImagePair FindMatchesBetweenTimeSeparatedFrames() {
+  using namespace cvp::vision;
+  cv::Mat image_left = app_data.time_separated_frames_.current_frame_;
+  cv::Mat image_right = app_data.time_separated_frames_.delayed_frame_;
+
+  visual_features::FeatureMatcher feature_matcher;
+  const auto feature_matches_left_right =
+      feature_matcher.FindCorrespondencesBetweenTwoImages(image_left, image_right);
+
+  return feature_matches_left_right;
+}
+
+void EstimateFundamentalMatrixBetweenConsecutiveFrames() {
+  const auto consecutive_frames = app_data.time_separated_frames_;
+  if (consecutive_frames.IsValid()) {
+    auto correspondences_between_images = FindMatchesBetweenTimeSeparatedFrames();
+    auto fundamental_matrix =
+        EstimateFundamentalMatrixFromMatches(correspondences_between_images);
+  
+    std::cout << fundamental_matrix << std::endl << std::endl;
+  }
+}
+
 int main(int argc, char *argv[]) {
   std::thread camera_thread(UpdateDataFromTimelapseCamera);
-  DrawFeatureMatchesOnMonocularStream();
+
+  while (true) {
+    EstimateFundamentalMatrixBetweenConsecutiveFrames();
+    DrawFeatureMatchesOnMonocularStream();
+  }
 
   camera_thread.join();
   return EXIT_SUCCESS;
